36fe2a743b74ae40401b42aed309f634d1343560
Traversable Dale <johnsyzonenko@gmail.com>
2025-11-01 16:49:19 -0400

    adds sample session | adds main | begins ext_summary to generate daily reports

---
+++ src/ext_summary.py
@@ -0,0 +1,21 @@
+class Summary :
+
+	def __innit__(self,ownerComp):
+		ownerComp = self.ownerComp
+		
+	def SummerizeSession(self):
+		#get session table
+		#op.summary.op('session')
+		#get file from op
+		
+		#Summerize data points
+		#i.e. events per app
+		#events per minute / second
+		# Breaks taken (define breaks as any time longer than 2 minutes without any activity)
+		# average break time
+		# number of clicks
+		# keyboard to click ratio
+		
+		print('Generating summary')
+		
+		return
